1688b3cd94488eacda2dd8f026a5f3bb72190bf5
Anshuman56 <anshumandehury@yandex.com>
2026-07-23 09:58:57 +0530

    build: mongoose conection and data geting form mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require("dotenv/config");
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const port = 3000;
@@ -6,38 +8,67 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-let todos = [{ id: 1, complited: true, todo: "dosomething" }];
-let nextId = 2;
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected");
+    const noteSchema = new mongoose.Schema({
+      title: String,
+      content: String,
+      complited: { type: Boolean, default: false },
+      createdAt: { type: Date, default: Date.now },
+    });
+
+    const Note = new mongoose.model("Note", noteSchema);
+
+    app.get("/todos", async (req, res) => {
+      try {
+        const notes = await Note.find();
+        res.send(notes);
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+
+    app.post("/todos", async (req, res) => {
+      try {
+        await Note.create({ title: "new mongoose", content: "more data" });
+        res.redirect("/todos");
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+
+    app.put("/todos/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const update = await Note.findById(id);
+        update.complited = !update.complited;
+        await update.save();
+        res.send(update);
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+
+    app.delete("/todos/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        await Note.findByIdAndDelete(id);
+        const todos = await Note.find();
+        res.send(todos);
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+main();
 
 app.get("/", (req, res) => {
   res.send("hello world");
-});
-
-app.get("/todos", (req, res) => {
-  res.send(todos);
-});
-
-app.post("/todos", (req, res) => {
-  req.body.id = nextId;
-  req.body.complited = false;
-  todos.push(req.body);
-  nextId++;
-  res.redirect("/todos");
-});
-
-app.put("/todos/:id", (req, res) => {
-  const id = Number(req.params.id);
-  console.log(id);
-  for (let i = 0; i < todos.length; i++) {
-    if (id === todos[i].id) todos[i].complited = !todos[i].complited;
-  }
-  res.send(todos);
-});
-
-app.delete("/todos/:id", (req, res) => {
-  const id = Number(req.params.id);
-  todos = todos.filter((item) => item.id !== id);
-  res.send(todos);
 });
 
 app.listen(port, () => {
